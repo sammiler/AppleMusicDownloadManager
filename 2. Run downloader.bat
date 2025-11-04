a85d10ec0824eaf3517279ca9387d28226d12ef6
sammiler <app.evergreen055@passinbox.com>
2025-11-04 13:29:04 +0800

    update python

--- 2. Run downloader.bat
+++ 2. Run downloader.bat
@@ -1,4 +1,4 @@
 @echo off
 cd apple-music-downloader
-..\wsl1\LxRunOffline.exe r -n u22-amdl  -c "PATH=/usr/local/go/bin:$PATH python3 -u controller.py"
+..\wsl1\LxRunOffline.exe r -n u22-amdl  -c "PATH=/usr/local/go/bin:$PATH /usr/bin/python3 -u controller.py"
 cmd /k
